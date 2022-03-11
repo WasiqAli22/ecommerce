fa571f891ad9127950998354e0ee5a511504f56d
WasiqAli <aliwasiq41@gmail.com>
2022-03-11 12:37:39 +0500

    Feat: DAY 2 -- Announcement Added
    Feat: DAY 2 -- Announcement Added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App
+# Ecommerce Application
 
 This is the ecommerce application which is made on ReactJS and Nodejs. In this website you can search,filter and order the products.
 
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Announcement from "./components/Announcement";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
     <div>
+      <Announcement/>
       <Navbar />
       {/* <HomePage/> */}
     </div>
