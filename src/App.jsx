8e7b87136202d365a65b3ae87fd42c3c1761f110
WasiqAli <aliwasiq41@gmail.com>
2022-03-21 12:36:29 +0500

    ProductList Page Content Added
    ProductList Page Content Added
    Along with Filteration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import HomePage from "./pages/HomePage";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <div>
+      <ProductList/>
       
-      <HomePage/>
+      {/* <HomePage/> */}
     </div>
   );
 }
