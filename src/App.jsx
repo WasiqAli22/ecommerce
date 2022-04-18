03a4253b9f18250858c8a30226e3d285dab1ee0b
WasiqAli <aliwasiq41@gmail.com>
2022-04-18 23:59:56 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import HomePage from "./pages/HomePage";
 import ProductDetail from "./pages/ProductDetail";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div>
-      <ProductDetail/>
+      <Register/>
     </div>
   );
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.4),
+      rgba(255, 255, 255, 0.4)
+    ),
+    url("https://www.gulahmedshop.com/static/version1647833451/frontend/Codazon/fastest_ellyana/en_US/Magento_Customer/images/bg1.jpg")
+      center;
+      /* " https://png.pngtree.com/thumb_back/fh260/background/20200805/pngtree-digital-connections-system-sphere-and-global-data-exchanges-on-black-and-image_383937.jpg" */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+`;
+const Wrapper = styled.div`
+  width: 40%;
+  background-color: white;
+  padding: 20px;
+`;
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+`;
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 20px 10px 0px 0px;
+    padding: 10px;
+`;
+const Agreement = styled.span`
+  font-size: 12px;
+  margin: 20px 0px;
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <Form>
+          <Input placeholder="first name" />
+          <Input placeholder="last name" />
+          <Input placeholder="username" />
+          <Input placeholder="email" />
+          <Input placeholder="password" />
+          <Input placeholder="confirm password" />
+          <Agreement>
+            By creating an account, I consent to the processing of my personal
+            data in accordance with the <b>PRIVACY POLICY</b>
+          </Agreement>
+          <Button>CREATE</Button>
+        </Form>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
