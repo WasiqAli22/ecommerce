0e5595c48176121e462ce8bd7f7905d5405351bd
WasiqAli <aliwasiq41@gmail.com>
2022-04-29 12:31:07 +0500

    LOGIN UI Completed
    LOGIN UI Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
 import ProductDetail from "./pages/ProductDetail";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
@@ -6,7 +7,7 @@ import Register from "./pages/Register";
 function App() {
   return (
     <div>
-      <Register/>
+      <Login/>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,72 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container=styled.div``;
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.2),
+      rgba(255, 255, 255, 0.2)
+    ),
+    url("https://png.pngtree.com/thumb_back/fh260/background/20200805/pngtree-digital-connections-system-sphere-and-global-data-exchanges-on-black-and-image_383937.jpg")
+      center;
+      /* " https://png.pngtree.com/thumb_back/fh260/background/20200805/pngtree-digital-connections-system-sphere-and-global-data-exchanges-on-black-and-image_383937.jpg" */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-size: cover;
+`;
+const Wrapper = styled.div`
+  width: 25%;
+  background-color: white;
+  padding: 20px;
+`;
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 300;
+`;
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+const Input = styled.input`
+    flex: 1;
+    min-width: 40%;
+    margin: 10px 0px;
+    padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  margin-bottom: 5px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+`;
+const Link=styled.a`
+   margin: 5px 0px;
+   font-size: 12px;
+   text-decoration:underline;
+   cursor: pointer;
+`;
 const Login = () => {
   return (
-    <Container>Login</Container>
+    <Container>
+      <Wrapper>
+        <Title>SIGN IN</Title>
+        <Form>
+          <Input placeholder="first name" />
+          <Input placeholder="password" />
+          <Button>SIGN IN</Button>
+          <Link>FORGET PASSWORD</Link>
+          <Link>CREATE AN ACCOUNT</Link>
+        </Form>
+      </Wrapper>
+    </Container>
   )
 }
 
